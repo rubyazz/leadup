bd3ede237ed6e7b71dff7ec3e12d95af41fc82c6
rubyazz <ersultan.abduvalov@gmail.com>
2023-01-30 21:16:06 +0600

    models, ckeditor

--- backend/base/app/admin.py
+++ backend/base/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Language, Chapter, Page
+
+
+admin.site.register(Language)
+admin.site.register(Chapter)
+admin.site.register(Page)
+
--- backend/base/app/models.py
+++ backend/base/app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+
+class Language(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Chapter(models.Model):
+    whichLanguage = models.ForeignKey(Language, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
+class Page(models.Model):
+    whichChapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    text = RichTextField(blank=True, null=True)
+    example = RichTextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
