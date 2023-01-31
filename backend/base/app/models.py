879568e2d4fdc8c92e52ffe02b2e5bf601789e4b
rubyazz <ersultan.abduvalov@gmail.com>
2023-01-31 23:09:40 +0600

    cool stuff

--- backend/base/app/models.py
+++ backend/base/app/models.py
@@ -2,19 +2,9 @@ from django.db import models
 from ckeditor.fields import RichTextField
 from django.urls import reverse
 
-# class Python(models.Model):
-#     name = models.CharField(max_length=200)
-#
-#     def __str__(self):
-#         return self.name
-#
-#     class Meta:
-#         verbose_name = "Python"
-#         verbose_name_plural = "Python"
 
 
 class Chapter(models.Model):
-    # whichLanguage = models.ForeignKey(Python, on_delete=models.CASCADE)
     name = models.CharField(max_length=150)
 
     def __str__(self):
@@ -32,5 +22,4 @@ class Page(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('page_detail', kwargs={'slug': self.slug})
-
+        return reverse('view_news', kwargs={"pk": self.pk})
